fixed IK formula, verified FK-IK roundtrip matches

kinematics/ik_analytic.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def inverse_kinematics(x,y,L1,L2,L3):
+def inverse_kinematics(x,y,L1,L2,L3,phi):
 
     """
     Computer theta1, theta2 , theta3 for 3DOF planar arm in radians
@@ -11,15 +11,16 @@ def inverse_kinematics(x,y,L1,L2,L3):
 
     return all joint angles 
     """
-    x_base,y_base=0,0
-
-    r = np.sqrt((x-x_base)**2 + (y-y_base)**2)
-
-    cos_theta2 = (L1**2+L2**2-r**2)/(2*L1*L2)
+    x_base,y_base=0,0  
+    wrist_x = x - L3*np.cos(phi)
+    wrist_y = y - L3*np.sin(phi)
+    r = np.sqrt((wrist_x-x_base)**2 + (wrist_y-y_base)**2)
+    
+    cos_theta2 = (r**2-L1**2-L2**2)/(2*L1*L2)
     cos_theta2 = np.clip(cos_theta2,-1,1)
     theta2 = np.arccos(cos_theta2)
-    theta1 = np.arctan2(y,x)-np.arctan2(L2*np.sin(theta2),L1+L2*np.cos(theta2))
+    theta1 = np.arctan2(wrist_y,wrist_x)-np.arctan2(L2*np.sin(theta2),L1+L2*np.cos(theta2))
 
-    theta3 = 0
+    theta3 = phi - theta1 - theta2
 
     return theta1, theta2, theta3