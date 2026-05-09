002a510d842ee7995e5c4d124dc0e1653b034d82
Riya Baranwal <ria.baranwal@gmail.com>
2026-05-08 21:04:26 -0400

    updated FK to return all joint positions for visualization

--- kinematics/fk.py
+++ kinematics/fk.py
@@ -11,9 +11,9 @@ def forward_kinematics(theta1, theta2, theta3, L1, L2, L3):
         (x,y): end-effector position
 
     """
-    base_x ,base_y =0,0
-    x1 = L1*np.cos(theta1) + base_x
-    y1 = L1*np.sin(theta1) + base_y
+    x_base ,y_base =0,0
+    x1 = L1*np.cos(theta1) + x_base
+    y1 = L1*np.sin(theta1) + y_base
 
     x2 = x1 + L2*np.cos(theta1+theta2)
     y2 = y1 + L2*np.sin(theta1+theta2)
@@ -21,4 +21,4 @@ def forward_kinematics(theta1, theta2, theta3, L1, L2, L3):
     x3 = x2 + L3*np.cos(theta1+theta2+theta3)
     y3 = y2 + L3*np.sin(theta1+theta2+theta3)
 
-    return x3, y3
+    return (x_base,y_base) , (x1,y1),(x2,y2),(x3,y3)
