fc39215fa1b23ff219aa2932046c338d2394795c
Riya Baranwal <ria.baranwal@gmail.com>
2026-05-09 23:17:54 -0400

    fixed dataset to use analytic IK labels - neural IK now matches analytic solution

--- ml/dataset.py
+++ ml/dataset.py
@@ -1,28 +1,29 @@
 import numpy as np
 import sys
-sys.path.append('..')
-from kinematics.fk import forward_kinematics
-
-def generate_dataset(n_samples):
+sys.path.insert(0, '/Users/riyabaranwal/kinematics-3dof')
+from kinematics.ik_analytic import inverse_kinematics
+def generate_dataset(n_samples, L1=1, L2=1, L3=1):
     """
-    This computes samples for the finding end effector position and orientation.
-    This calls forward kinematics with random values of thetas and returns x,y and phi
+    Generate dataset using analytic IK for consistent labels.
+    Input: random (x, y, phi)
+    Output: analytic IK solution (theta1, theta2, theta3)
     """
-
-    theta1 = np.random.uniform(-np.pi,np.pi,n_samples)
-    theta2 = np.random.uniform(-np.pi,np.pi,n_samples)
-    theta3 = np.random.uniform(-np.pi,np.pi,n_samples)
-    inputs=[]
-    output =[]
-
-    for i in range(n_samples):
-        phi=theta1[i]+theta2[i]+theta3[i]
-
-        (_,_),(_,_),(_,_),(x,y)=forward_kinematics(theta1=theta1[i],theta2=theta2[i],theta3=theta3[i],L1=1,L2=1,L3=1)
-        inputs.append((x,y,phi))
-        output.append((theta1[i],theta2[i],theta3[i]))
- 
-    inputs =np.array(inputs)
-    output = np.array(output)
-
-    return inputs, output
+    inputs = []
+    outputs = []
+    
+    count = 0
+    while count < n_samples:
+        x = np.random.uniform(-2, 2)
+        y = np.random.uniform(-2, 2)
+        phi = np.random.uniform(-np.pi, np.pi)
+        
+        try:
+            t1, t2, t3 = inverse_kinematics(x, y, L1, L2, L3, phi)
+            if not any(np.isnan([t1, t2, t3])):
+                inputs.append([x, y, phi])
+                outputs.append([t1, t2, t3])
+                count += 1
+        except:
+            continue
+    
+    return np.array(inputs), np.array(outputs)
